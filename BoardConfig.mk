8e90fc6bd3539a6c2732e9cc908bec60bb8fc19c
Khalvat-M <mmba313@gmail.com>
2020-02-02 15:22:38 +0330

    Match with new BoardConfig.mk Map

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 # inherit from common kltechn
-include device/samsung/klte-common/BoardConfigCommon.mk
+include device/samsung/klte-common/BoardConfig.mk
 
 TARGET_OTA_ASSERT_DEVICE := klte,kltechn,kltexx,kltezm,kltezn
 
